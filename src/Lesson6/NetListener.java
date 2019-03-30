472113a2ba9f403d866aa18ac4441f3d19f926c7
Dmalson <dmalov42@gmail.com>
2019-03-30 19:24:38 +0500

    Lesson 6, edition 2.0

--- src/Lesson6/NetListener.java
+++ src/Lesson6/NetListener.java
@@ -1,5 +1,38 @@
 package Lesson6;
 
-public class NetListener {
+import java.io.DataInputStream;
 
+public class NetListener implements Runnable {
+    private DataInputStream dataInputStream;
+
+    public NetListener(DataInputStream dataInputStream) {
+        this.dataInputStream = dataInputStream;
+    }
+
+    public NetListener() {
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message = null;
+            while (true) {
+                try {
+                    message = dataInputStream.readUTF();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    break;
+                }
+                if (message.equalsIgnoreCase("exit")) {
+                    System.out.println("Собеседник покинул беседу.");
+                    break;
+                } else System.out.println("Собеседник: " + message);
+            }
+            dataInputStream.close();
+            System.out.println("Service NetListener closed");
+        }  catch (Exception e) {
+            System.out.println("Ошибка сервиса NetListener");
+            e.printStackTrace();
+        }
+    }
 }
